6c0d015d2e3886a054b414ccb7273a6ad878e127
hmetaute <hernan.metaute@gmail.com>
2013-04-08 10:51:39 -0500

    Primer intento de subida a Heroku del API de OpenMRS

---
+++ src/com/heroku/HelloWorld.java
@@ -0,0 +1,38 @@
+package com.heroku;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorld extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1358822218306947685L;
+	
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		resp.getWriter().print("Hello world, madafaka");
+	}
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(new HelloWorld()),"/*");
+        server.start();
+        server.join();   
+	}
+
+}
